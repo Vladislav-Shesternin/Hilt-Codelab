3942c1c070a2396314f444ff5e5470f476ca918f
Vlad Shesternin <valdon1202@gmail.com>
2021-03-22 19:00:02 +0200

    Done Hilt: [@HiltAndroidApp, @AndroidEntryPoint, @InstallIn, Components, @Singleton, @Module, @Provides, @Binds, Qualifier, Context].

--- app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
@@ -30,14 +30,17 @@ import com.example.android.hilt.R
 import com.example.android.hilt.data.Log
 import com.example.android.hilt.data.LoggerLocalDataSource
 import com.example.android.hilt.util.DateFormatter
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-/**
- * Fragment that displays the database logs.
- */
+@AndroidEntryPoint
 class LogsFragment : Fragment() {
 
-    private lateinit var logger: LoggerLocalDataSource
-    private lateinit var dateFormatter: DateFormatter
+    @Inject
+    lateinit var logger: LoggerLocalDataSource
+
+    @Inject
+    lateinit var dateFormatter: DateFormatter
 
     private lateinit var recyclerView: RecyclerView
 
@@ -55,18 +58,6 @@ class LogsFragment : Fragment() {
         }
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        populateFields(context)
-    }
-
-    private fun populateFields(context: Context) {
-        logger = (context.applicationContext as LogApplication).serviceLocator.loggerLocalDataSource
-        dateFormatter =
-            (context.applicationContext as LogApplication).serviceLocator.provideDateFormatter()
-    }
-
     override fun onResume() {
         super.onResume()
 
@@ -80,9 +71,6 @@ class LogsFragment : Fragment() {
     }
 }
 
-/**
- * RecyclerView adapter for the logs list.
- */
 private class LogsViewAdapter(
     private val logsDataSet: List<Log>,
     private val daterFormatter: DateFormatter
